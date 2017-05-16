8b15187fb885597cdc69c926ded39d2991fcee15
Christof Stocker <stocker.christof@gmail.com>
2017-05-16 17:09:44 +0200

    Augmentor: update readme script

--- Augmentor/readme_isic.jl
+++ Augmentor/readme_isic.jl
@@ -6,14 +6,13 @@ srand(123)
 
 img = get(ImageThumbnailRequest(id = "5592ac599fc3c13155a57a85"))
 
-pipeline = (
-    Either(1=>FlipX(), 1=>FlipY(), 2=>NoOp()),
-    Rotate(0:360),
-    Either(ShearX(-5:5), ShearY(-5:5)),
-    CropSize(165, 165),
-    Zoom(1:0.05:1.2),
+pipeline =
+    Either(1=>FlipX(), 1=>FlipY(), 2=>NoOp()) |>
+    Rotate(0:360) |>
+    ShearX(-5:5) * ShearY(-5:5) |>
+    CropSize(165, 165) |>
+    Zoom(1:0.05:1.2) |>
     Resize(64, 64)
-)
 
 # Create image that shows the input
 plot(img, size=(256,169), xlim=(1,255), ylim=(1,168), grid=false, ticks=true)
